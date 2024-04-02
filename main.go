9501f6ae1f166020993584cad88a7aeb25f850d1
markgerald <mark.g.martins@gmail.com>
2024-04-02 14:54:58 -0300

    modifying handler

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	orderRepository := repository.NewOrdersRepositoryImpl(DB)
 	orderService := service.NewOrderServiceImpl(orderRepository, validate)
 	orderController := controller.NewOrderController(orderService)
-	ginLambda := router.NewRouter(orderController)
+	ginLambda = router.NewRouter(orderController) // Removed the ":" to assign to the package-level ginLambda
 	if ginLambda == nil {
 		log.Fatal("Failed to initialize ginLambda")
 	}
